630e6827a8339f4778cc9502d56aa925281e22b0
NaturPelmem <kirillov240503@gmail.com>
2024-04-25 08:58:04 +0500

    1.2

--- main/admin.py
+++ main/admin.py
@@ -4,26 +4,51 @@ from .models import *
 
 @admin.register(Competition)
 class CompetitionAdmin(admin.ModelAdmin):
-    list_display = ('title', 'programs_start', 'programs_end', 'description', 'slug')
+    list_display = ('title', 'programs_start', 'programs_end', 'description', 'contacts', 'age')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('competition', 'rating', 'created_at')
-    list_filter = ('competition', 'rating')
-    search_fields = ('competition__title', 'comment')
+    list_display = ('rating', 'comment', 'created_at')
+    filter_horizontal = ('lesson',)
+    search_fields = ('comment',)
 
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'start', 'description', 'competition')
-    list_filter = ('competition',)
+    list_display = ('title', 'description', 'contacts')
     search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
 
 
 @admin.register(Contacts)
 class ContactsAdmin(admin.ModelAdmin):
     list_display = ('address', 'phone', 'mail')
     search_fields = ('address', 'phone', 'mail')
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'age', 'having_place', 'duration_lesson', 'cost_lesson', 'repetitions_week', 'duration_training')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Age)
+class AgeAdmin(admin.ModelAdmin):
+    list_display = ('participants',)
+    search_fields = ('participants',)
+
+
+@admin.register(Having)
+class HavingAdmin(admin.ModelAdmin):
+    list_display = ('place',)
+    search_fields = ('place',)
+
+
+@admin.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('duration',)
+    search_fields = ('duration',)
--- main/models.py
+++ main/models.py
@@ -9,8 +9,8 @@ class Competition(models.Model):
     description = models.TextField('Описание')
     image = models.ImageField('Изображение', upload_to='competition_images/')
     contacts = models.ForeignKey('Contacts', verbose_name="Контакты", on_delete=models.CASCADE)
+    age = models.ForeignKey('Age', verbose_name="Возраст", on_delete=models.CASCADE)
     slug = AutoSlugField(populate_from='title', unique=True, editable=True, db_index=True, always_update=True)
-
 
     def __str__(self):
         return f'{self.title} - {self.programs_start} - {self.programs_end} - {self.slug}'
@@ -20,32 +20,15 @@ class Competition(models.Model):
         verbose_name_plural = "Соревнования"
 
 
-class Review(models.Model):
-    competition = models.ForeignKey(Competition, verbose_name="Соревнование", on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField('Рейтинг')
-    comment = models.TextField('Комментарий')
-    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.competition.title} - {self.rating}'
-
-    class Meta:
-        verbose_name = "Отзыв"
-        verbose_name_plural = "Отзывы"
-
-
 class News(models.Model):
     title = models.CharField('Название', max_length=100)
-    start = models.DateTimeField('Дата начала')
     description = models.TextField('Описание')
     image = models.ImageField('Изображение', upload_to='news_images/')
-    competition = models.ForeignKey(Competition, verbose_name="Соревнование", on_delete=models.CASCADE,
-                                    related_name='news', blank=True, null=True)
     contacts = models.ForeignKey('Contacts', verbose_name="Контакты", on_delete=models.CASCADE)
     slug = AutoSlugField(populate_from='title', unique=True)
 
     def __str__(self):
-        return f'{self.start} - {self.description}'
+        return f'{self.title} - {self.description}'
 
     class Meta:
         verbose_name = "Новость"
@@ -58,8 +41,74 @@ class Contacts(models.Model):
     mail = models.EmailField('Почта')
 
     def __str__(self):
-        return f'{self.address} - {self.phone}'
+        return f'{self.address} - {self.phone} - {self.mail}'
 
     class Meta:
         verbose_name = "Контакт"
         verbose_name_plural = "Контакты"
+
+
+class Lesson(models.Model):
+    title = models.CharField('Название', max_length=100)
+    age = models.ForeignKey('Age', verbose_name="Возраст участника", on_delete=models.CASCADE)
+    having_place = models.ForeignKey('Having', verbose_name="Длительность занятия", on_delete=models.CASCADE)
+    duration_lesson = models.ForeignKey('Time', verbose_name="Наличие мест", on_delete=models.CASCADE)
+    cost_lesson = models.CharField('Стоимость', max_length=100)
+    repetitions_week = models.CharField('Занятий в неделю', max_length=100)
+    duration_training = models.CharField('Длительность обучения', max_length=100)
+    description = models.TextField('Описание')
+    slug = AutoSlugField(populate_from='title', unique=True, editable=True, db_index=True, always_update=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.age} - {self.having_place} - {self.duration_lesson}'
+
+    class Meta:
+        verbose_name = "Соревнование"
+        verbose_name_plural = "Соревнования"
+
+
+class Review(models.Model):
+    rating = models.PositiveSmallIntegerField('Рейтинг')
+    comment = models.TextField('Комментарий')
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
+    lesson = models.ManyToManyField('Lesson', related_name='Review')
+
+    def __str__(self):
+        return f'{self.comment} - {self.rating}'
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+
+class Age(models.Model):
+    participants = models.CharField('Возраст', max_length=50)
+
+    def __str__(self):
+        return f'{self.participants}'
+
+    class Meta:
+        verbose_name = "Врзраст участника"
+        verbose_name_plural = "Возраст участников"
+
+
+class Having(models.Model):
+    place = models.CharField('Наличие', max_length=50)
+
+    def __str__(self):
+        return f'{self.place}'
+
+    class Meta:
+        verbose_name = "Наличие мест"
+        verbose_name_plural = "Наличие мест"
+
+
+class Time(models.Model):
+    duration = models.CharField('Длительность', max_length=50)
+
+    def __str__(self):
+        return f'{self.duration}'
+
+    class Meta:
+        verbose_name = "Длительность занятия"
+        verbose_name_plural = "Длительность занятия"
